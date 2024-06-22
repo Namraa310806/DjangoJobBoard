878b69c0912e911417d60f3e33d5a87f60e672cf
Namraa Patel <patelnamraa310806@gmail.com>
2024-06-22 16:32:41 +0530

    Detail page of jobs added

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,6 +5,7 @@ from jobs import views
 urlpatterns = [
     path('',views.home),
     path('jobs/',views.jobs),
-    path('form/',views.form)
+    path('form/',views.form),
+    path('jobs/<str:slug>',views.detail)
 
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -7,11 +7,14 @@ from jobs.models import Job,Application
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
 def jobs(request):
     job=Job.objects.all()
     context = {'job':job}
     return render(request,"jobs.html",context)
+
 def form(request):
+    context={'success':False}
     if request.method == "POST":
         full_name=request.POST['full_name']
         email=request.POST['email']
@@ -20,5 +23,11 @@ def form(request):
         resume=request.POST['resume']
         cover_letter=request.POST['cover_letter']
         linkedin=request.POST['linkedin']
-        job=Job(full_name=full_name,email=email,phone=phone,position=position,resume=resume,cover_letter=cover_letter,linkein=linkedin)
-    return render(request,"form.html")
+        applicant=Application(full_name=full_name,email=email,phone=phone,position=position,resume=resume,cover_letter=cover_letter,linkedin=linkedin)
+        applicant.save()
+        context={'success':True}
+    return render(request,"form.html",context)
+
+def detail(request,slug):
+    job=Job.objects.filter(Company=slug)
+    return render(request,'detail.html',{'job':job})
