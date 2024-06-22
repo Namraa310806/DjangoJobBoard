b4ac082f5e55bd82898156a12db90314a56f2d7c
Namraa Patel <patelnamraa310806@gmail.com>
2024-06-22 13:58:14 +0530

    application connected to db

--- jobs/views.py
+++ jobs/views.py
@@ -12,4 +12,13 @@ def jobs(request):
     context = {'job':job}
     return render(request,"jobs.html",context)
 def form(request):
+    if request.method == "POST":
+        full_name=request.POST['full_name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        position=request.POST['position']
+        resume=request.POST['resume']
+        cover_letter=request.POST['cover_letter']
+        linkedin=request.POST['linkedin']
+        job=Job(full_name=full_name,email=email,phone=phone,position=position,resume=resume,cover_letter=cover_letter,linkein=linkedin)
     return render(request,"form.html")
